79c41b8a6df9236f2d1d294fb08b951edf47fafd
Sudeep Mishra <sudeep912.kumar@gmail.com>
2026-08-27 12:26:33 +0530

    TCP username and password

--- Core/Src/telnet_server.c
+++ Core/Src/telnet_server.c
@@ -2,6 +2,7 @@
 #include "lwip/err.h"
 #include "lwip/pbuf.h"
 #include "lwip/mem.h"
+#include <string.h>
 
 #define TELNET_PORT 23
 
@@ -10,19 +11,33 @@
 #define TELNET_WONT  252
 #define TELNET_DO    253
 #define TELNET_DONT  254
+#define TELNET_ECHO  1
 
 #define TELNET_STATE_DATA       0
 #define TELNET_STATE_IAC        1
 #define TELNET_STATE_COMMAND    2
 
+#define TELNET_LOGIN_USERNAME      0
+#define TELNET_LOGIN_PASSWORD      1
+#define TELNET_LOGIN_AUTHENTICATED 2
+
 struct telnet_client
 {
     uint8_t telnet_state;
     uint8_t telnet_command;
+
+    uint8_t login_state;
+
+    char username[32];
+    uint8_t username_len;
+
+    char password[32];
+    uint8_t password_len;
+
+    uint8_t last_was_cr;
 };
 
-static err_t telnet_recv(void *arg, struct tcp_pcb *tpcb,
-                         struct pbuf *p, err_t err)
+static err_t telnet_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
 {
     struct telnet_client *client = (struct telnet_client *)arg;
     uint8_t *data;
@@ -68,11 +83,100 @@ static err_t telnet_recv(void *arg, struct tcp_pcb *tpcb,
                 }
                 else
                 {
-                    /* Normal character: echo it */
-                    tcp_write(tpcb,
-                              &data[i],
-                              1,
-                              TCP_WRITE_FLAG_COPY);
+                    /*
+                     * Ignore LF if it immediately follows CR.
+                     * Terminals commonly send Enter as CR + LF.
+                     */
+                    if (data[i] == '\n' && client->last_was_cr)
+                    {
+                        client->last_was_cr = 0;
+                        continue;
+                    }
+
+                    if (data[i] == '\r')
+                    {
+                        client->last_was_cr = 1;
+                    }
+                    else
+                    {
+                        client->last_was_cr = 0;
+                    }
+
+                    if (client->login_state == TELNET_LOGIN_USERNAME)
+                    {
+                        if (data[i] == '\r' || data[i] == '\n')
+                        {
+                            static const char password_prompt[] = "\rPassword: ";
+
+                            client->username[client->username_len] = '\0';
+                            client->password_len = 0;
+
+                            uint8_t echo_off[] =
+                            {
+                                TELNET_IAC,
+                                TELNET_WILL,
+                                TELNET_ECHO
+                            };
+
+                            tcp_write(tpcb, echo_off, sizeof(echo_off), TCP_WRITE_FLAG_COPY);
+
+                            tcp_write(tpcb, password_prompt, sizeof(password_prompt) - 1, TCP_WRITE_FLAG_COPY);
+
+                            tcp_output(tpcb);
+
+                            client->login_state = TELNET_LOGIN_PASSWORD;
+                        }
+                        else if (client->username_len < sizeof(client->username) - 1)
+                        {
+                            client->username[client->username_len++] = data[i];
+                        }
+                    }
+                    else if (client->login_state == TELNET_LOGIN_PASSWORD)
+                    {
+                        if (data[i] == '\r' || data[i] == '\n')
+                        {
+                            client->password[client->password_len] = '\0';
+
+                            if (strcmp(client->username, "sudeepk") == 0 && strcmp(client->password, "coral@auth@123") == 0)
+                            {
+                                static const char login_ok[] = "\r\nLogin successful.\r\n";
+
+                                uint8_t echo_restore[] =
+                                {
+                                    TELNET_IAC,
+                                    TELNET_WONT,
+                                    TELNET_ECHO
+                                };
+
+                                tcp_write(tpcb, echo_restore, sizeof(echo_restore), TCP_WRITE_FLAG_COPY);
+
+                                tcp_write(tpcb, login_ok, sizeof(login_ok) - 1, TCP_WRITE_FLAG_COPY);
+
+                                client->login_state = TELNET_LOGIN_AUTHENTICATED;
+                            }
+                            else
+                            {
+                                static const char login_failed[] = "\r\nLogin incorrect.\r\nUsername: ";
+
+                                client->username_len = 0;
+                                client->password_len = 0;
+
+                                tcp_write(tpcb, login_failed, sizeof(login_failed) - 1, TCP_WRITE_FLAG_COPY);
+
+                                client->login_state = TELNET_LOGIN_USERNAME;
+                            }
+
+                            tcp_output(tpcb);
+                        }
+                        else if (client->password_len < sizeof(client->password) - 1)
+                        {
+                            client->password[client->password_len++] = data[i];
+
+                            /*
+                             * Do NOT echo password characters.
+                             */
+                        }
+                    }
                 }
 
                 break;
@@ -81,17 +185,11 @@ static err_t telnet_recv(void *arg, struct tcp_pcb *tpcb,
 
                 if (data[i] == TELNET_IAC)
                 {
-                    tcp_write(tpcb,
-                              &data[i],
-                              1,
-                              TCP_WRITE_FLAG_COPY);
+                    tcp_write(tpcb, &data[i], 1, TCP_WRITE_FLAG_COPY);
 
                     client->telnet_state = TELNET_STATE_DATA;
                 }
-                else if (data[i] == TELNET_WILL ||
-                         data[i] == TELNET_WONT ||
-                         data[i] == TELNET_DO ||
-                         data[i] == TELNET_DONT)
+                else if (data[i] == TELNET_WILL || data[i] == TELNET_WONT || data[i] == TELNET_DO || data[i] == TELNET_DONT)
                 {
                     client->telnet_command = data[i];
                     client->telnet_state = TELNET_STATE_COMMAND;
@@ -107,24 +205,75 @@ static err_t telnet_recv(void *arg, struct tcp_pcb *tpcb,
             {
                 uint8_t response[3];
 
-                response[0] = TELNET_IAC;
-
-                if (client->telnet_command == TELNET_WILL ||
-                    client->telnet_command == TELNET_WONT)
+                /*
+                 * ECHO negotiation is special because the server
+                 * initiates it when entering password mode.
+                 */
+                if (data[i] == TELNET_ECHO)
                 {
-                    response[1] = TELNET_DONT;
+                    if (client->telnet_command == TELNET_DO)
+                    {
+                        /*
+                         * Client accepted our WILL ECHO.
+                         *
+                         * Nothing else needs to be sent.
+                         */
+                    }
+                    else if (client->telnet_command == TELNET_DONT)
+                    {
+                        /*
+                         * Client rejected ECHO.
+                         * Tell the client we will not echo.
+                         */
+                        response[0] = TELNET_IAC;
+                        response[1] = TELNET_WONT;
+                        response[2] = TELNET_ECHO;
+
+                        tcp_write(tpcb,
+                                  response,
+                                  sizeof(response),
+                                  TCP_WRITE_FLAG_COPY);
+                    }
+                    else if (client->telnet_command == TELNET_WILL ||
+                             client->telnet_command == TELNET_WONT)
+                    {
+                        /*
+                         * We don't want the client controlling echo.
+                         */
+                        response[0] = TELNET_IAC;
+                        response[1] = TELNET_DONT;
+                        response[2] = TELNET_ECHO;
+
+                        tcp_write(tpcb,
+                                  response,
+                                  sizeof(response),
+                                  TCP_WRITE_FLAG_COPY);
+                    }
                 }
                 else
                 {
-                    response[1] = TELNET_WONT;
+                    /*
+                     * We don't support other Telnet options.
+                     */
+                    response[0] = TELNET_IAC;
+
+                    if (client->telnet_command == TELNET_WILL ||
+                        client->telnet_command == TELNET_WONT)
+                    {
+                        response[1] = TELNET_DONT;
+                    }
+                    else
+                    {
+                        response[1] = TELNET_WONT;
+                    }
+
+                    response[2] = data[i];
+
+                    tcp_write(tpcb,
+                              response,
+                              sizeof(response),
+                              TCP_WRITE_FLAG_COPY);
                 }
-
-                response[2] = data[i];
-
-                tcp_write(tpcb,
-                          response,
-                          sizeof(response),
-                          TCP_WRITE_FLAG_COPY);
 
                 client->telnet_state = TELNET_STATE_DATA;
                 client->telnet_command = 0;
@@ -166,6 +315,20 @@ static err_t telnet_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
 
     client->telnet_state = TELNET_STATE_DATA;
     client->telnet_command = 0;
+
+    client->login_state = TELNET_LOGIN_USERNAME;
+    client->username_len = 0;
+    client->password_len = 0;
+    client->last_was_cr = 0;
+
+    static const char username_prompt[] = "Username: ";
+
+    tcp_write(newpcb,
+              username_prompt,
+              sizeof(username_prompt) - 1,
+              TCP_WRITE_FLAG_COPY);
+
+    tcp_output(newpcb);
 
     tcp_arg(newpcb, client);
     tcp_recv(newpcb, telnet_recv);
